f923b709df9d6f84ce0b72968ddff610fb1a07e0
dudu <fandazeng@gmail.com>
2021-03-27 13:55:07 +0800

    增加自定义图标工具类

---
+++ lib/utils/MyIcons.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+
+/// 自定义字体图标
+class MyIcons {
+  // 人体检测
+  static const IconData BodyCheck =
+      const IconData(0xec5a, fontFamily: "myIcon", matchTextDirection: true);
+
+  // 汽车
+  static const IconData Car =
+      const IconData(0xec6d, fontFamily: "myIcon", matchTextDirection: true);
+
+  // 定位
+  static const IconData Location =
+      const IconData(0xe7af, fontFamily: "myIcon2", matchTextDirection: true);
+
+}
